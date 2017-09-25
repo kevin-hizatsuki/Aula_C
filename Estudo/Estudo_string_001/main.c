0f6f1a09ebccc4098a625ddbb35f1a6dcefaa30e
kevin799 <kevinmikio23@gmail.com>
2017-09-25 16:08:12 -0300

    Uso de string v010
    Inserindo uma função contador de digitos numericos

--- Estudo/Estudo_string_001/main.c
+++ Estudo/Estudo_string_001/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <ctype.h>  //Para o uso de isdigit na função strcountd
 #define DIM 20
 #define SEP_NOME ", "
+
 
 int strcountc(char *s,char ch)
 {
@@ -11,10 +13,19 @@ int strcountc(char *s,char ch)
     return conta;
 }
 
+int strcountd(char *s)
+{
+    int i,conta;
+    for(i=conta=0;s[i]!='\0';i++)
+        if(isdigit(s[i]))
+        conta++;
+    return conta;
+}
+
 main ()
 {
     char Nome[DIM+1],Sobrenome[DIM+1],Completo[2*DIM+1];
-    int rep;
+    int rep,contdig;
     while (1)
     {
         printf("Nome: ");gets(Nome);
@@ -25,6 +36,11 @@ main ()
         strcat(Completo,Nome);
         rep=strcountc(Completo,'e');
         puts(Completo);
-        printf("O numero que se repete e igual a: %i",rep);
+        printf("O numero que se repete e igual a: %i\n",rep);
+        contdig=strcountd(Completo);
+        if (contdig!=0)
+            printf("Este string contem %i numeros\n",contdig);
+        else
+            printf("Nao ha digitos numerico nessa string\n");
     }
 }
