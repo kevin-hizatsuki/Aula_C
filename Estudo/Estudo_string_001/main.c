a688e4439b958f3e7e3d858000d877e09318afc4
kevin799 <kevinmikio23@gmail.com>
2017-09-25 15:46:17 -0300

    Uso de string v008
    Aplicação do strcpy e strcat

--- Estudo/Estudo_string_001/main.c
+++ Estudo/Estudo_string_001/main.c
@@ -1,30 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int isnull(char *s)
+#define DIM 20
+#define SEP_NOME ", "
+main ()
 {
-    return(s[0]=='\0');
-}
-
-
-
-main()
-{
-    char nome[100],nomeCopy[100];
-    while(1)
+    char Nome[DIM+1],Sobrenome[DIM+1],Completo[2*DIM+1];
+    while (1)
     {
-        puts("Nome: ");
-        gets(nome);
-        printf("\nTamanho do nome: %i\n",strlen(nome));
-        if (isnull(nome)==0){
-            puts("True");
-        }else{puts("False");}
-        if (nome[0]=='\0')
-            break;
-        else
-            printf("Nome introduzido: %s\n",nome);
-            strcpy(nomeCopy,nome);
-            printf("Copia do nome: %s",nomeCopy);
+        printf("Nome: ");gets(Nome);
+        if(strlen(Nome)==0) break;
+        printf("Sobrenome: ");gets(Sobrenome);
+        strcpy(Completo,Sobrenome);
+        strcat(Completo,SEP_NOME);
+        strcat(Completo,Nome);
+        puts(Completo);
     }
 }
