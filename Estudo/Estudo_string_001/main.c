6ede56fadb2ea34226607c399c0a4ce7f54665ce
kevin799 <kevinmikio23@gmail.com>
2017-09-25 15:55:20 -0300

    Uso de string v009
    Implementação do strcountc(Contador de caracter que se repete)

--- Estudo/Estudo_string_001/main.c
+++ Estudo/Estudo_string_001/main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #define DIM 20
 #define SEP_NOME ", "
+
+int strcountc(char *s,char ch)
+{
+    int i,conta;
+    for (i=conta=0;s[i]!='\0';i++)
+        if (s[i]==ch)
+        conta++;
+    return conta;
+}
+
 main ()
 {
     char Nome[DIM+1],Sobrenome[DIM+1],Completo[2*DIM+1];
+    int rep;
     while (1)
     {
         printf("Nome: ");gets(Nome);
@@ -12,6 +23,8 @@ main ()
         strcpy(Completo,Sobrenome);
         strcat(Completo,SEP_NOME);
         strcat(Completo,Nome);
+        rep=strcountc(Completo,'e');
         puts(Completo);
+        printf("O numero que se repete e igual a: %i",rep);
     }
 }
