79b2dd68a38106e0ef31a69c31145e208d99d9a3
kevin799 <kevinmikio23@gmail.com>
2017-09-25 16:48:12 -0300

    Uso de string v011
    Inserindo a função que pesquisa a posição de um determinado caracter

--- Estudo/Estudo_string_001/main.c
+++ Estudo/Estudo_string_001/main.c
@@ -22,25 +22,48 @@ int strcountd(char *s)
     return conta;
 }
 
+int indchr(char *s,char ch)
+{
+    int i;
+    for (i=0;s[i]!='\0';i++)
+        if (s[i]==ch)
+        return i;
+    return -1;
+}
+
 main ()
 {
-    char Nome[DIM+1],Sobrenome[DIM+1],Completo[2*DIM+1];
-    int rep,contdig;
+    char Nome[DIM+1],Sobrenome[DIM+1],Completo[2*DIM+1],ch;
+    int rep,contdig,valCh;
     while (1)
     {
         printf("Nome: ");gets(Nome);
         if(strlen(Nome)==0) break;
         printf("Sobrenome: ");gets(Sobrenome);
+        printf("Insira o caracter que deseja pesquisar a posicao: ");scanf("%c",&ch);
         strcpy(Completo,Sobrenome);
         strcat(Completo,SEP_NOME);
         strcat(Completo,Nome);
         rep=strcountc(Completo,'e');
+        valCh=indchr(Completo,ch);
         puts(Completo);
         printf("O numero que se repete e igual a: %i\n",rep);
         contdig=strcountd(Completo);
         if (contdig!=0)
+        {
             printf("Este string contem %i numeros\n",contdig);
+        }
         else
+        {
             printf("Nao ha digitos numerico nessa string\n");
+        }
+        if(valCh!=-1)
+        {
+            printf("O caracter %c esta na posicao %i\n",ch,valCh);
+        }
+        else
+        {
+            printf("O caracter %c nao se encontra no string\n",ch);
+        }
     }
 }
