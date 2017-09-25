61e2f4df8f05f491377618c0bfa35c6b20ee3bae
kevin799 <kevinmikio23@gmail.com>
2017-09-25 13:56:06 -0300

    Uso de string_v001
    uso de scanf junto a string

---
+++ Estudo/Estudo_string_001/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+    char nome[50],sobrenome[50];
+    printf("Introduza o nome: ");scanf("%s",nome);
+    printf("Introduza o sobrenome: ");scanf("%s",sobrenome);
+    printf("Nome completo: %s %s ",nome,sobrenome);
+}
