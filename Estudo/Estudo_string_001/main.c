7e040ad4a53bcbf25f1744010c558fdbdb2a871b
kevin799 <kevinmikio23@gmail.com>
2017-09-25 14:04:41 -0300

    Uso_de_string_v002
    Tentativa de printar uma string

--- Estudo/Estudo_string_001/main.c
+++ Estudo/Estudo_string_001/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 main()
 {
-    char nome[50],sobrenome[50];
-    printf("Introduza o nome: ");scanf("%s",nome);
-    printf("Introduza o sobrenome: ");scanf("%s",sobrenome);
-    printf("Nome completo: %s %s ",nome,sobrenome);
+    char nome[50];
+    printf("Introduza o nome completo: ");scanf("%s",nome);
+    printf("Nome completo: %s\n",nome);
 }
