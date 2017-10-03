27fedd8781fff7868f6a05bc0403878ddfc35b1e
kevin799 <kevinmikio23@gmail.com>
2017-10-03 16:10:45 -0300

    inclusao do srand(time(NULL))

--- Estudo/Numeros aleatorios/main.c
+++ Estudo/Numeros aleatorios/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
     int i;
 
     printf("Gerando 10 numeros aleatorios: \n\n");
+
+    srand(time(NULL));
+
     for(i=0;i<10;i++)
     {
         printf("%d ",rand()%100);
