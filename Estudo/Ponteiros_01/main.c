ca03d47c64743a5484b2d6d13a0ee0304c9e9674
kevin799 <kevinmikio23@gmail.com>
2017-10-22 16:46:50 -0200

    Aula_001
    Emdereço de memoria

--- Estudo/Ponteiros_01/main.c
+++ Estudo/Ponteiros_01/main.c
@@ -6,9 +6,10 @@ main()
     int x;
     x=10;
     int *ponteiro;
-    ponteiro = &x;
-    printf("%i\n",*ponteiro); //Com o asterisco acessa-se o valor do x.
-    printf("%i\n",ponteiro);  //Sem o asterisco acessa-se o valor do endereço de momoria do x.
+    ponteiro= &x;
+    int y = 20;
+    *ponteiro = y; //Ela atibui ao valor da memoria do x o valor de y.
 
+    printf("%i %i\n", x,y);
 
 }
