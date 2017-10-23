b9337f6ea78d845abf5bb6aa25e78fe29cb5a11f
kevin799 <kevinmikio23@gmail.com>
2017-10-23 15:26:56 -0200

    Aula_004
    Simplificação de estrutura

--- Estudo/Estruturas/Estruturas/main.c
+++ Estudo/Estruturas/Estruturas/main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct horario
+/*struct horario
     {
         int horas;
         int minutos;
         int segundos;
         double teste;
         char letra;
-    };
+    };*/
 
 int main()
 {
-    struct horario funcao(struct horario x);
-
+    //struct horario funcao(struct horario x);
+    struct horario
+    {
+        int horas;
+        int minutos;
+        int segundos;
+        double teste;
+        char letra;
+    }exemplo={10,20,51};
     struct horario agora;
     agora.horas=13;
     agora.minutos=17;
@@ -30,19 +37,21 @@ int main()
     printf("%i\n",depois.minutos);
     printf("%f\n",depois.teste);
     printf("%c\n",depois.letra);
+    printf("Exemplo : %i:%i:%i\n",exemplo.horas,exemplo.minutos,exemplo.segundos);
 
-    struct horario proximo;
+
+    /*struct horario proximo;
     proximo = funcao(agora);
 
     printf("%i:%i:%i\n",proximo.horas,proximo.minutos,proximo.segundos);
-
+    */
 
     return 0;
 }
-struct horario funcao(struct horario x){
+/*struct horario funcao(struct horario x){
     printf("%i:%i:%i\n",x.horas,x.minutos,x.segundos);
     x.horas=100;
     x.minutos=100;
     x.segundos=100;
     return x;
-};
+};*/
