29f71c7459c85ae0e4a1ec3d10f552748c6ebd53
kevin799 <kevinmikio23@gmail.com>
2017-10-23 13:44:33 -0200

    Aula_002
    Implementação de outros tipos de variaveis dentro de uma estrutura

--- Estudo/Estruturas/Estruturas/main.c
+++ Estudo/Estruturas/Estruturas/main.c
@@ -8,6 +8,8 @@ int main()
         int horas;
         int minutos;
         int segundos;
+        double teste;
+        char letra;
     };
 
     struct horario agora;
@@ -15,7 +17,17 @@ int main()
     agora.minutos=17;
     agora.segundos=12;
 
+    struct horario depois;
+    depois.horas = agora.horas + 10;
+    depois.minutos = agora.minutos;
+    depois.teste = 50.22/6;
+    depois.letra = 'a';
+
     printf("%i:%i:%i\n",agora.horas,agora.minutos,agora.segundos);
+    printf("%i\n",depois.horas);
+    printf("%i\n",depois.minutos);
+    printf("%f\n",depois.teste);
+    printf("%c\n",depois.letra);
 
     return 0;
 }
